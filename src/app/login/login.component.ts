251589299e5339f58ae09b9ba3f3195e735c3afe
hansdeep <hansdeep.singh@hotmail.com>
2021-11-05 10:29:53 +1100

    New Responsive Design Orange and Blue

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,6 @@ import {
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"],
   animations: [
     trigger("labelAnimate", [
       state(
